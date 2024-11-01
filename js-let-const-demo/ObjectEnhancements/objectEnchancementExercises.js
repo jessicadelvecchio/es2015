dcbac44dc31689fcc8369edca9118afa0454157f
Jessica DelVecchio <hey@jdvdv.com>
2024-11-01 16:35:15 -0700

    exercises done

--- js-let-const-demo/ObjectEnhancements/objectEnchancementExercises.js
+++ js-let-const-demo/ObjectEnhancements/objectEnchancementExercises.js
@@ -1,54 +1,74 @@
 // **Object Enhancements Exercise**
 // In this exercise, you’ll refactor some ES5 code into ES2015. Write your code in the sections with a comment to “Write an ES2015 Version”.
 
-// TODO **Same keys and values
-function createInstructor(firstName, lastName) {
-  return {
-    firstName: firstName,
-    lastName: lastName,
-  };
-}
+//**Same keys and values
+// function createInstructor(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//   };
+// }
 
-//**Same keys and values ES2015**
 /* Write an ES2015 Version */
 
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
 //
 //
 //
 //
 
-// TODO **Computed Property Names**
+// **Computed Property Names**
 
-var favoriteNumber = 42;
+// var favoriteNumber = 42;
 
-var instructor = {
+// var instructor = {
+//   firstName: "Colt",
+// };
+
+// instructor[favoriteNumber] = "That is my favorite!";
+
+/* Write an ES2015 Version */
+
+let favoriteNumber = 42;
+
+const instructor = {
   firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!",
 };
 
 instructor[favoriteNumber] = "That is my favorite!";
+//
+//
+//
+//
 
-// **Computed Property Names ES2015**
+// ** Object Methods
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function () {
+//     return "Hi!";
+//   },
+//   sayBye: function () {
+//     return this.firstName + " says bye!";
+//   },
+// };
+
 /* Write an ES2015 Version */
-
-//
-//
-//
-//
-
-// TODO ** Object Methods
-
-var instructor = {
+const instructor = {
   firstName: "Colt",
-  sayHi: function () {
+  sayHi() {
     return "Hi!";
   },
-  sayBye: function () {
+  sayBye() {
     return this.firstName + " says bye!";
   },
 };
-
-//** Object Methods ES2015
-/* Write an ES2015 Version */
 
 // TODO createAnimal function**
 
@@ -67,3 +87,22 @@ d.bark(); //"Woooof!"
 const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
 // {species: "sheep", bleet: ƒ}
 s.bleet(); //"BAAAAaaaa"
+
+// function createAnimal(species, verb, noise) {
+//     return {
+//       [species]: species,
+//       [verb]: verb,
+//       [noise]: noise,
+//     };
+//   }
+
+// ! correction
+function createAnimal(species, verb, noise) {
+  return {
+    species, // shorthand property name
+    [verb]() {
+      // computed property name and shorthand methods
+      return noise;
+    },
+  };
+}
