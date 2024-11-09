3a41d6a8ca342deb529853c552824ffbb68334e8
Jessica DelVecchio <hey@jdvdv.com>
2024-11-08 17:10:30 -0800

    maps and sets notes

--- js-let-const-demo/MapsAndSets/maps.js
+++ js-let-const-demo/MapsAndSets/maps.js
@@ -1,3 +1,26 @@
+// ** map basics
+
+// finding the size is easy, no more loops or object.keys
+// keys can be any data type
+
+// maps are like objects. in objects all keys are turned into strings
+// IN MAPS keys can be any data type, objects, functions, booleans, etc
+
+// how do we make them?
+// must use `new`
+
+const myMap = new Map(); // create an empty map
+myMap.set(7, "seven"); // add key/value pairs to the map using set (key, value) data types will stay the same, will NOT be changed to strings
+myMap.set("7", "seven string");
+
+const empty = [];
+//myMap.set([], "empty array"); // an empty arr or obj will be undefined because it is pointing to a reference number, not the arr/obj itself. to set a key/value as a obj/arr need to first save as a variable
+myMap.set(empty, "empty array"); // this will have empty arr as key
+
+myMap.get(); // to get information/key values from a map. cannot use myMap[key] will be undefined.
+
+// when using map can only get keys/values if the data type is correct. unlike objs which convert all keys and values to strings.
+
 // const myMap = new Map();
 // myMap.set(7, 'seven');
 // myMap.set('7', 'seven string');
@@ -8,31 +31,76 @@
 
 // myMap.get(7);
 
+// ** using functions in a map
+
 // const add = (x, y) => x + y;
 // const mult = (x, y) => x * y;
 
 // const funcCalls = new Map();
-// funcCalls.set(add, 0);
+// funcCalls.set(add, 0); // storing functions as keys with init val of 0
 // funcCalls.set(mult, 0);
 
-// funcCalls.set(add, 1);
+// funcCalls.set(add, 1); // later these can update and using .get() will show change
 // funcCalls.set(mult, 9);
 
+// ** using methods with maps
+// not that many but already: .set(), .get()
+
+// ** creating a map without using new Map()
+
+// pass in an arr with sub arrays that contain [key, val]
+
 const bandData = [
-	[ 3, '3 Doors Down' ],
-	[ 'three', 'Three Dog Night' ],
-	[ 'nine', 'Nine Inch Nails' ],
-	[ 'four', 'The Four Seasons' ],
-	[ 41, 'Sum 41' ]
+  // arr of arr
+  [3, "3 Doors Down"], // [key, value]
+  ["three", "Three Dog Night"],
+  ["nine", "Nine Inch Nails"],
+  ["four", "The Four Seasons"],
+  [41, "Sum 41"],
 ];
+
+// save map in variable, still using new Map(arrName)
 const bandMap = new Map(bandData);
 
-bandMap.set(182, 'Blink-182').set('twenty', 'Matchbox Twenty');
+// ** adding key/val pair by using set. Can chain set calls together
+// will mutate bandMap
+bandMap.set(182, "Blink-182").set("twenty", "Matchbox Twenty");
 
-// bandMap.forEach((val, key) => {
+// ** checking if a map contains something
+//bandMap.has(9); // false
+//bandMap.has("nine"); // true
+
+// ** delete()
+//bandMap.delete("twenty"); // delete that key/val pair
+
+// ** clear()
+//bandMap.clear(); // clears map entirely
+
+// ** .keys(), .values()
+// shows all keys or values
+// can iterate through these
+// can use spread to create an arr
+//[...bandMap.keys()]
+
+// ** looping with maps, iterating
+// maps are in added order
+
+// ** maps have size, not length
+bandMap.size;
+
+// ** forEach
+
+// bandMap.forEach((val, key) => { // val first, then key
 // 	console.log(key + '=>' + val);
 // });
 
-for (let [ key, value ] of bandMap) {
-	console.log(key, '=', value);
+// ** for of
+
+for (let [key, value] of bandMap) {
+  // key first, then val
+  // use destructuring to iterate through key val
+  console.log(key, "=", value);
 }
+
+// ** can change a map back to an arr using the spread operator
+[...bandMap];
--- js-let-const-demo/MapsAndSets/sets.js
+++ js-let-const-demo/MapsAndSets/sets.js
@@ -1,26 +1,56 @@
-// To create an empty Set:
+// sets do not have key val pairs
+// all values in a set are unique
+// any data type is allowed
+
+// removing dup values
+// uniqueness is required
+// efficient checking if an item is in a collection, better than arrays
+
+// **To create an empty Set:
 new Set();
 
-//You can also pass in an iterable (like an array):
-const bannedHashTags = new Set([ 'nofilter', 'justsaying', 'winning', 'yolo' ]);
+// **You can also pass in an iterable (like an array):
+// can only pass in one iterable
+// set will seperate each val and only keep unique values
+const bannedHashTags = new Set(["nofilter", "justsaying", "winning", "yolo"]);
 
-// To add to a Set:
-bannedHashTags.add('bestlife').add('selfie');
+// ** To add to a Set:
+// if something is already in the set, will ignore, will only add unique values
+// can chain adds
+bannedHashTags.add("bestlife").add("selfie");
+
+// ** check if a value is a set
+// returns boolean value
+bannedHashTags.has("yolo"); // true
+bannedHashTags.has("tbt"); // false
+
+// ** .delete() and .clear()
+// delete single value
+// clear entire set
+
+// ** using a set within a function
+// sets and arrays work similarly
+// but sets ensure no duplicates
+// sets are more efficient, fast at adding new values and checking values within an arr
 
 function filterHashTags(tags) {
-	const bannedHashTags = [ 'nofilter', 'justsaying', 'winning', 'yolo' ];
-	return tags.filter((tag) => !bannedHashTags.has(tag));
+  const bannedHashTags = ["nofilter", "justsaying", "winning", "yolo"];
+  return tags.filter((tag) => !bannedHashTags.has(tag));
 }
 
-const susansTags = [ 'happymonday', 'yolo', 'winning', 'sunset' ];
+// returns any tags not inside of a set
+const susansTags = ["happymonday", "yolo", "winning", "sunset"];
 filterHashTags(susansTags); //[ 'happymonday', 'sunset' ];
 
-//Removing duplicates using a Set:
-const ages = [ 45, 42, 21, 23, 24, 98, 2, 4, 4, 12, 3, 12, 45 ];
-[ ...new Set(ages) ]; //create a new Set and then spread it back into an Array
+// ** Removing duplicates using a Set:
+const ages = [45, 42, 21, 23, 24, 98, 2, 4, 4, 12, 3, 12, 45];
+[...new Set(ages)]; //create a new Set and then spread it back into an Array
 
-// Iterating Sets:
+// ** Iterating Sets:
 for (let tag of bannedHashTags) {
-	console.log('STOP USING:', tag);
+  console.log("STOP USING:", tag);
 }
+
+// using arrow functions
+
 // bannedHashTags.forEach((tag) => console.log('STOP USING:', tag));
